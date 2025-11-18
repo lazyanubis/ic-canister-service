9c3594bddebb2c63714a1b49c9df309e78fba8bd
anubis <lazyanubis@users.noreply.github.com>
2025-11-18 10:51:36 +0800

    fix: refactor business trait

--- src/stable/business.rs
+++ src/stable/business.rs
@@ -9,56 +9,74 @@ pub use std::collections::{HashMap, HashSet};
 #[allow(unused)]
 pub use std::fmt::Display;
 
-#[allow(unused_variables)]
-pub trait Business:
-    Pausable<PauseReason>
-    + ParsePermission
-    + Permissable<Permission>
-    + Recordable<Record, RecordTopic, RecordSearch>
-    + Schedulable
-    + ScheduleTask
-    + StableHeap
-{
-    fn business_example_query(&self) -> String {
-        ic_cdk::trap("Not supported operation by this version.")
-    }
-    fn business_example_count_query(&self) -> u64 {
-        ic_cdk::trap("Not supported operation by this version.")
-    }
-}
+pub(super) mod immutable {
+    use super::*;
 
-#[allow(clippy::panic)] // ? 允许回滚
-#[allow(clippy::unwrap_used)] // ? 允许回滚
-#[allow(clippy::expect_used)] // ? 允许回滚
-#[allow(unused_variables)]
-pub trait MutableBusiness: Business {
-    fn business_example_update(&mut self, test: String) {
-        ic_cdk::trap("Not supported operation by this version.")
+    pub trait GetImmutable {
+        fn get(&self) -> &dyn Business;
     }
-    fn business_example_count_update(&mut self, value: u64) {
-        ic_cdk::trap("Not supported operation by this version.")
-    }
-}
 
-// 业务实现
-impl Business for State {
-    fn business_example_query(&self) -> String {
-        self.get().business_example_query()
+    #[allow(unused_variables)]
+    pub trait Business:
+        Pausable<PauseReason>
+        + ParsePermission
+        + Permissable<Permission>
+        + Recordable<Record, RecordTopic, RecordSearch>
+        + Schedulable
+        + ScheduleTask
+        + StableHeap
+    {
+        fn business_example_query(&self) -> String {
+            ic_cdk::trap("Not supported operation by this version.")
+        }
+        fn business_example_count_query(&self) -> u64 {
+            ic_cdk::trap("Not supported operation by this version.")
+        }
     }
-    fn business_example_count_query(&self) -> u64 {
-        self.get().business_example_count_query()
-    }
-}
 
-// 业务实现
-#[allow(clippy::panic)] // ? 允许回滚
-#[allow(clippy::unwrap_used)] // ? 允许回滚
-#[allow(clippy::expect_used)] // ? 允许回滚
-impl MutableBusiness for State {
-    fn business_example_update(&mut self, test: String) {
-        self.get_mut().business_example_update(test)
-    }
-    fn business_example_count_update(&mut self, value: u64) {
-        self.get_mut().business_example_count_update(value)
+    // 业务实现
+    impl Business for State {
+        fn business_example_query(&self) -> String {
+            self.get().business_example_query()
+        }
+        fn business_example_count_query(&self) -> u64 {
+            self.get().business_example_count_query()
+        }
     }
 }
+pub use immutable::Business;
+
+pub mod mutable {
+    use super::*;
+
+    pub trait GetMutable {
+        fn get_mut(&mut self) -> &mut dyn MutableBusiness;
+    }
+
+    #[allow(clippy::panic)] // ? 允许回滚
+    #[allow(clippy::unwrap_used)] // ? 允许回滚
+    #[allow(clippy::expect_used)] // ? 允许回滚
+    #[allow(unused_variables)]
+    pub trait MutableBusiness: Business {
+        fn business_example_update(&mut self, test: String) {
+            ic_cdk::trap("Not supported operation by this version.")
+        }
+        fn business_example_count_update(&mut self, value: u64) {
+            ic_cdk::trap("Not supported operation by this version.")
+        }
+    }
+
+    // 业务实现
+    #[allow(clippy::panic)] // ? 允许回滚
+    #[allow(clippy::unwrap_used)] // ? 允许回滚
+    #[allow(clippy::expect_used)] // ? 允许回滚
+    impl MutableBusiness for State {
+        fn business_example_update(&mut self, test: String) {
+            self.get_mut().business_example_update(test)
+        }
+        fn business_example_count_update(&mut self, value: u64) {
+            self.get_mut().business_example_count_update(value)
+        }
+    }
+}
+pub use mutable::MutableBusiness;
